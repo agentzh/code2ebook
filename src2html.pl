76f08e7acddb73c90375366c05334666035894f6
Yichun Zhang (agentzh) <agentzh@gmail.com>
2014-12-31 21:15:09 -0800

    feature: now we use ctags to generate TOC links for function definitions at the beginning of each source file.

--- src2html.pl
+++ src2html.pl
@@ -7,10 +7,69 @@ use warnings;
 sub process_dir ($);
 sub write_src_html ($$);
 sub write_index ($$);
+sub shell ($);
+sub process_tags ($);
+sub add_elem_to_hash ($$$);
+sub is_tag_array ($);
+sub gen_func_link ($$$$);
+sub extract_line_by_lineno ($$$$);
+
+#my %tag_by_files;
+#my %tag_by_names;
+my %func_by_files;
 
 my $dir = shift or die "No source directory specified.\n";
 my $pkg_name = shift or die "No book title specified.\n";
+shell "ctags -n -u --fields=+l --c-kinds=+l -R '$dir'";
+process_tags("./tags");
 process_dir($dir);
+
+sub shell ($) {
+    my $cmd = shift;
+    system($cmd) == 0
+        or die qq{failed to run command "$cmd": $!\n};
+}
+
+sub process_tags ($) {
+    my $tagfile = shift;
+    open my $in, $tagfile
+        or die "cannot open $tagfile for reading: $!\n";
+    while (<$in>) {
+        next if /^!/;
+        if (/^([^\t]+)\s*\t([^\t]+)\t(\d+);"\t(\S+)\tlanguage:(\S+)/) {
+            my ($name, $file, $lineno, $kind, $lang) = ($1, $2, $3, $4, $5);
+            if ($lang eq 'C++') {
+                $lang = 'C';
+            }
+            #warn "name=$name, file=$file, lineno=$lineno, kind=$kind, lang=$lang\n";
+            $name =~ s/^\s+|\s+$//g;
+            my $rec = [$name, $file, $lineno, $kind, $lang];
+            #add_elem_to_hash(\%tag_by_files, $file, $rec);
+            #add_elem_to_hash(\%tag_by_names, $name, $rec);
+            if ($kind eq 'f') {
+                add_elem_to_hash(\%func_by_files, $file, $rec);
+            }
+
+        } else {
+            die "Unknown tags file line: ", quotemeta($_);
+        }
+    }
+    close $in;
+}
+
+sub add_elem_to_hash ($$$) {
+    my ($hash, $key, $val) = @_;
+    my $old = $hash->{$key};
+    if (!defined $old) {
+        $hash->{$key} = $val;
+
+    } elsif (ref $old && ref $old eq 'ARRAY' && !ref $old->[0]) {
+        $hash->{$key} = [$old, $val];
+
+    } else {
+        push @$old, $val;
+    }
+}
 
 sub process_dir ($) {
     my $dir = shift;
@@ -46,25 +105,68 @@ sub write_src_html ($$) {
     my ($dir, $entity) = @_;
 
     my $infile = "$dir/$entity";
-    #my $src;
+    warn "Reading source file $infile\n";
+
     open my $in, $infile or
         die "Can't open $infile for reading: $!\n";
-    local $/;
-    my $src = <$in>;
+    my @lineno_index;
+    my $src = '';
+    my $pos = 0;
+    while (<$in>) {
+        $lineno_index[$.] = $pos;
+        $src .= $_;
+        $pos += length;
+    }
     close $in;
+
+    my $preamble = '';
+    my $tag = $func_by_files{$infile};
+    if (defined $tag) {
+        $preamble .= <<_EOC_;
+ <h4>Functions defined</h4>
+ <ul>
+_EOC_
+        if (is_tag_array($tag)) {
+            my $tags = $tag;
+            @$tags = sort { $a->[0] cmp $b->[0] } @$tags;
+            for my $t (@$tags) {
+                my $rc = gen_func_link(\$preamble, $t, \$src, \@lineno_index);
+                if (!$rc) {
+                    undef $t;
+                }
+            }
+
+            for my $t (@$tags) {
+                next unless defined $t;
+                tag_line_by_lineno(\$src, $t->[1], $t->[2], \@lineno_index);
+            }
+
+        } else {
+            gen_func_link(\$preamble, $tag, \$src, \@lineno_index);
+            tag_line_by_lineno(\$src, $tag->[1], $tag->[2], \@lineno_index);
+        }
+
+        $preamble .= <<_EOC_;
+ </ul>
+ <h4>Source code</h4>
+_EOC_
+    }
 
     if ($src =~ /PRETEND TO BE IN Parse::RecDescent NAMESPACE/s) {
         $src = 'Omitted parser file generated automatically by Parse::RecDescent';
     } else {
-        $src =~ s/\n\n\n+/\n\n/gs;
-        $src =~ s/[ \t]+\n/\n/gs;
-        $src =~ s/\t/    /gs;
-        $src =~ s/\&/\&amp;/g;
-        $src =~ s/ /&nbsp;/gs;
-        $src =~ s/</\&lt;/g;
-        $src =~ s/>/\&gt;/g;
-        $src =~ s/"/\&quot;/g;
-        $src =~ s/\n/<br\/>/g;
+        for ($src) {
+            s/\n\n\n+/\n\n/gs;
+            s/[ \t]+\n/\n/gs;
+            s/\t/    /gs;
+            s/\&/\&amp;/g;
+            s/ /&nbsp;/gs;
+            s/</\&lt;/g;
+            s/>/\&gt;/g;
+            s/"/\&quot;/g;
+            s{_SRC2KINDLE_L(\d+)_}{<a id="_L$1"></a>}smg;
+            s/\n/<br\/>/g;
+        }
     }
 
     my $outfile = "$dir/$entity.html";
@@ -77,12 +179,76 @@ sub write_src_html ($$) {
  </head>
  <body>
   <h3>$infile - $pkg_name</h3>
+$preamble
   <code>$src</code>
  </body>
 </html>
 _EOC_
     close $out;
     warn "Wrote $outfile\n";
+}
+
+sub gen_func_link ($$$$) {
+    my ($preamble_ref, $tag, $src_ref, $lineno_index) = @_;
+    my $name = $tag->[0];
+    my $file = $tag->[1];
+    my $lineno = $tag->[2];
+    my $lang = $tag->[4];
+    if ($lang eq 'C' && $name =~ /^[_A-Z]+$/) {
+        # possibly a macro-sugared C function; use the whole line
+        $name = extract_line_by_lineno($src_ref, $file, $lineno,
+                                       $lineno_index);
+        #die "Found line at line $lineno: [$name]";
+        if ($name =~ /^\s*case\b.*?:/) {
+            return undef;
+        }
+    }
+    $$preamble_ref .= <<_EOC_;
+<li><a href="#_L$lineno">$name</a></li>
+_EOC_
+    return 1;
+}
+
+sub extract_line_by_lineno ($$$$) {
+    my ($src_ref, $file, $lineno, $lineno_index) = @_;
+    my $pos = $lineno_index->[$lineno];
+    if (!defined $pos) {
+        die "Line $lineno not found in file $file (only seen ",
+            scalar(@$lineno), " lines)\n";
+    }
+    #warn "setting pos to $pos...\n";
+    pos $$src_ref = $pos;
+    if ($$src_ref =~ /\G[^\n]*/m) {
+        my $line = $&;
+        #if ($file eq 'src/lib_ffi.c') {
+        #warn "$file:$lineno:$pos: [$line]\n";
+        #}
+        return $line;
+    }
+}
+
+sub tag_line_by_lineno ($$$$) {
+    my ($src_ref, $file, $lineno, $lineno_index) = @_;
+    my $pos = $lineno_index->[$lineno];
+    if (!defined $pos) {
+        die "Line $lineno not found in file $file (only seen ",
+            scalar(@$lineno), " lines)\n";
+    }
+    pos $$src_ref = $pos;
+    my $replace = "_SRC2KINDLE_L${lineno}_";
+    if ($$src_ref =~ s/\G/$replace/sm) {
+        my $offset = length $replace;
+        for (my $i = $lineno + 1; 1; $i++) {
+            my $pos = $lineno_index->[$i];
+            last if !defined $pos;
+            $lineno_index->[$i] += $offset;
+        }
+    }
+}
+
+sub is_tag_array ($) {
+    my ($t) = @_;
+    ref $t && ref $t eq 'ARRAY' && ref $t->[0];
 }
 
 sub write_index ($$) {
