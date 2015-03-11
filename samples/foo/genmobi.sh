57bf8002c535314fb961b8cde2437ee2bf6e805d
Yichun Zhang (agentzh) <agentzh@gmail.com>
2015-03-11 16:39:39 -0700

    updated the sample to reflect recent changes.

--- samples/foo/genmobi.sh
+++ samples/foo/genmobi.sh
@@ -13,7 +13,18 @@ title="foo program"
 export PATH=$PWD/../..:$PATH
 
 echo "Generating HTMLs from ./ for $name..."
-src2html.pl --color --cross-reference --line-numbers . $name
+src2html.pl --navigator --color --cross-reference --line-numbers . $name
+
+echo "Generating .pdf file for $name..."
+rm -rf *.pdf
+ebook-convert html_out/index.html $name.pdf \
+	--override-profile-size \
+	--paper-size a4 \
+	--pdf-default-font-size 12 \
+	--pdf-mono-font-size 12 \
+	--margin-left 10 --margin-right 10 \
+	--margin-top 10 --margin-bottom 10 \
+	--page-breaks-before='/'
 
 echo "Generating $name.epub file..."
 rm -rf *.epub
